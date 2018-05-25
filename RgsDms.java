b90cd8fc3d338e529a8c559b2d44ca64cac3a0f1
crazymaza <dx2389@gmail.com>
2018-05-25 23:17:52 +0200

    Final version

--- RgsDms.java
+++ RgsDms.java
@@ -6,15 +6,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.util.concurrent.TimeUnit;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
 public class RgsDms {
     private WebDriver driver;
     private WebDriverWait wait;
+    private Select select;
+    private WebElement element;
 
     @Before
     public void start(){
@@ -23,31 +24,67 @@ public class RgsDms {
         options.setBinary("C://Program Files (x86)//Google//Chrome//Application");//Указываю путь к браузеру
         driver = new ChromeDriver();
         wait = new WebDriverWait(driver, 20);
+        driver.manage().window().maximize();
     }
+
+     private void getSelect(WebElement element){
+        select = new Select(element);
+     }
 
     @Test
     public void rgsDms() throws Exception {
+        firstPage();
+        secondPage();
+        thirdPage();
+        modalWindow();
+    }
+
+    private void firstPage(){
         driver.get("http://www.rgs.ru");
         wait.until(titleIs("Росгосстрах – крупнейшая страховая компания России с 1921 года"));
 //        WebElement element = driver.findElement(By.className("text-center"));
 //        String txt = element.getText();
-        WebElement element = driver.findElement(By.xpath("//div[@class = 'row text-center']" +
+        element = driver.findElement(By.xpath("//div[@class = 'row text-center']" +
                 "/div[@class = 'col-xs-4 col-sm-3 col-lg-2']/a[contains(text(),'Здоровье')]"));
         element.click();
-        wait.until(titleIs("Страхование здоровья"));
-        WebElement element1 = driver.findElement(By.cssSelector(".list-group.list-group-rgs-menu.collapse a:first-child"));
-        element1.click();
-        wait.until(titleIs("ДМС 2018 \uD83D\uDE91 Добровольное медицинское страхование, рассчитать стоимость и оформить ДМС в Росгосстрахе"));
-        WebElement element2 = driver.findElement(By.cssSelector(".rgs-context-bar-content-call-to-action-buttons a:nth-child(4)"));
-        element2.click();
-        WebElement element3 = driver.findElement(By.className("modal-content"));
-        WebElement elementLname = element3.findElement(By.name("LastName"));
-        elementLname.sendKeys("Педрововочкин");
-        WebElement elementFname = element3.findElement(By.name("FirstName"));
-        elementFname.sendKeys("Педро");
-        WebElement elementMname = element3.findElement(By.name("MiddleName"));
-        elementFname.sendKeys("Педрович");
+    }
 
+    private void secondPage(){
+        wait.until(titleIs("Страхование здоровья"));
+        element = driver.findElement(By.cssSelector(".list-group.list-group-rgs-menu.collapse" +
+                " a:first-child"));
+        element.click();
+    }
+
+    private void thirdPage(){
+        wait.until(titleIs("ДМС 2018 \uD83D\uDE91 Добровольное медицинское страхование," +
+                " рассчитать стоимость и оформить ДМС в Росгосстрахе"));
+        element = driver.findElement(By.cssSelector(".rgs-context-bar-content-call-to-action-buttons" +
+                " a:nth-child(4)"));
+        element.click();
+    }
+
+    private void modalWindow(){
+        element = driver.findElement(By.className("modal-content"));
+        WebElement modalElement = element.findElement(By.name("LastName"));
+        modalElement.sendKeys("Педрововочкин");
+        modalElement = element.findElement(By.name("FirstName"));
+        modalElement.sendKeys("Педро");
+        modalElement = element.findElement(By.name("MiddleName"));
+        modalElement.sendKeys("Педрович");
+        modalElement = element.findElement(By.name("Region"));
+        getSelect(modalElement);
+        select.selectByVisibleText("Москва");
+//        Номер телефона не получилось сделать
+//        modalElement = element.findElement(By.cssSelector("input:nth-child(5)"));
+//        modalElement = element.findElement(By.cssSelector(".form-control.validation-control-has-error"));
+//        modalElement = element.findElement(By.xpath("//input[@class = " +
+//                "'.form-control.validation-control-has-error']"));
+//        modalElement.sendKeys("9455554545");
+        modalElement = element.findElement(By.id("button-m"));
+        if (modalElement.isEnabled()){
+            System.out.println("Кнопка доступна, тест не пройден.");
+        }
 
     }
 
